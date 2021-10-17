b8a0931166d82296daec0157f0da1f43286cf7cc
Meletis Krassas <meletiskrassas@outlook.com.gr>
2021-10-17 19:14:59 +0300

    update App js and css to include header component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component.jsx';
 
 const HatsPage = () => (
   <div>
@@ -14,9 +16,10 @@ const HatsPage = () => (
 function App() {
   return ( 
     <div>
+        <Header />
         <switch>
             <Route exact path='/' component={HomePage} /> 
-            <Route path='/shop/hats' component={HatsPage} />
+            <Route path='/shop/' component={ShopPage} />
         </switch>
     </div> 
   ); 
